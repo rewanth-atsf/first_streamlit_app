1d048e6ca7feb21f5f85480e165403d6e752fef6
rewanth-atsf <134135794+rewanth-atsf@users.noreply.github.com>
2023-05-21 17:27:10 +0530

    Update streamlit_app.py
    View Our Fruit List - Add Your Favorites!

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
 ####################Let's Query Our Trial Account Metadata####################
 
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 # snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
