20b750fee87bdb0e85e74ab7d2a5a19d14275f02
rewanth-atsf <134135794+rewanth-atsf@users.noreply.github.com>
2023-05-21 16:12:33 +0530

    Update streamlit_app.py
    from_streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,9 +56,10 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
-#################### Adding A Second Text Entry Box ####################
+### Adding A Second Text Entry Box ###
 
 # Allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
