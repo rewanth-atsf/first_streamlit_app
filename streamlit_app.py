e3f90a75748bc550385cf4657854f556f2cbad4c
rewanth-atsf <134135794+rewanth-atsf@users.noreply.github.com>
2023-05-21 17:06:15 +0530

    Update streamlit_app.py
    Commented line STOP streamlit function

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,8 +62,8 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
   
 
-  # don't run anything past here while we troubleshoot
-streamlit.stop()
+# don't run anything past here while we troubleshoot
+#streamlit.stop()
 
 
 ### Adding A Second Text Entry Box ###
