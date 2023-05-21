e4a17e0a4c1422f5c41ed2a5512a2a3d0b259067
rewanth-atsf <134135794+rewanth-atsf@users.noreply.github.com>
2023-05-21 15:32:15 +0530

    Update streamlit_app.py
    Let's Query Our Trial Account Metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,15 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+####################Let's Query Our Trial Account Metadata####################
+
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
